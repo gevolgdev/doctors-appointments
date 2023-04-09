dccd891066c0cf96d034b2a405f13f2b6118c847
Pinelli <ricardosilvapinelli@gmail.com>
2023-04-09 02:41:05 -0300

    Pagina Mark: Iniciada

--- src/routes/Mark/index.tsx
+++ src/routes/Mark/index.tsx
@@ -1,9 +1,81 @@
-import { MarkContainer } from "./style";
+import { useState } from 'react'
+import { MarkContainer, MarkContent } from "./style";
+
+interface TypeProps {
+  id: number;
+  option: string;
+};
+
+interface AppointmentsProps {
+  doctor: string;
+  subject: string;
+  date: string;
+  time: string;
+}
 
 const Mark = () => {
+
+  const [appointments, setAppointments] = useState<AppointmentsProps | null>(null);
+
+  function handleAddAppointments() {
+    
+  }
+
+  const doctors: TypeProps[] = [
+    {id: 1, option: 'Ricardo'},
+    {id: 2, option: 'Rodolfo'},
+    {id: 3, option: 'José'},
+    {id: 4, option: 'Rodrigo'},
+  ];
+
+  const subjects: TypeProps[] = [
+    { id: 1, option: 'Cardiologia' },
+    { id: 2, option: 'Dermatologia' },
+    { id: 3, option: 'Ginecologia' },
+    { id: 4, option: 'Pediatria' },
+  ];
+
   return (
-    <MarkContainer>Mark</MarkContainer>
+    <MarkContainer>
+      <MarkContent>
+        <h1>Agende sua consulta</h1>
+
+        <div className="inputs-content">
+          <span>Médico</span>
+          <select>
+            <option value="" disabled selected>-----</option>
+            {doctors.map((doctor) => (
+              <option key={doctor.id} value={doctor.option}>
+                {doctor.option}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="inputs-content">
+          <span>Assunto</span>
+          <select>
+            <option value="" disabled selected>-----</option>
+            {subjects.map((subject) => (
+              <option key={subject.id} value={subject.option}>
+                {subject.option}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="inputs-content">
+          <span>Data</span>
+          <input type="date"/>
+        </div>
+
+        <div className="inputs-content">
+          <span>Hora</span>
+          <input type="time"/>
+        </div>
+      </MarkContent>
+    </MarkContainer>
   )
 }
 
-export default Mark
+export default Mark;
--- src/routes/Mark/style.ts
+++ src/routes/Mark/style.ts
@@ -1,5 +1,53 @@
 import styled from "styled-components";
 
 export const MarkContainer = styled.section`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+export const MarkContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 80px;
+  gap: 30px;
+
+  h1 {
+    font-size: 3rem;
+    color: white;
+  }
+
+  .inputs-content {
+    display: flex;
+    flex-direction: column;
+
+    span{ font-size: .8rem; color: white; opacity: 80%; margin-bottom: 5px;}
+
+    select {
+      width: 400px;
+      height: 52px;
+      padding: 0 15px;
+      outline: none;
+      border: solid 2px #F1EFF1;
+      font-size: 16px;
+      background: #F7F5F7;
+      border-radius: 10px;
   
-`
+      option {
+        font-size: 14px;
+      }
+    }
+
+    input {
+      width: 400px;
+      height: 52px;
+      padding: 0 15px;
+      outline: none;
+      border: solid 2px #F1EFF1;
+      font-size: 16px;
+      font-family: 'Inter', sans-serif;
+      background: #F7F5F7;
+      border-radius: 10px;
+    }
+  }
+`;
