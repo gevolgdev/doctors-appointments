bbda2582ad88ba1da9209ca964ffff7f50d1c22e
Pinelli <ricardosilvapinelli@gmail.com>
2023-04-10 21:32:23 -0300

    Dashboard

--- src/routes/Dashboard/index.tsx
+++ src/routes/Dashboard/index.tsx
@@ -1,6 +1,53 @@
+import { useSelector } from 'react-redux'
+import { useAppointments } from '../../redux/sliceAppointments';
+import { DashContainer, DashContent, Patient } from './style';
+import { AppointmentsProps } from '../../types/types';
+
+
 const Dashboard = () => {
+
+  const appointments = useSelector(useAppointments);
+
+  const newAppointments: AppointmentsProps[] = appointments.slice(1)
+
+  console.log(newAppointments)
+
   return (
-    <div>Dashboard</div>
+    <DashContainer>
+      <DashContent>
+        <h1>Consultas agendadas</h1>
+        {
+          newAppointments.length <= 0
+          ? <h1 className='no-content'>Não há consultas marcadas!</h1> 
+          : (
+            newAppointments.map(appointments => (
+              <Patient>
+                <div className='content'>
+                  <span>Paciente</span>
+                  <h3>{appointments.name}</h3>
+                </div>
+    
+                <div className='line'/>
+    
+                <div className='content'>
+                  <span>Sobre</span>
+                  <h3>
+                    {appointments.subject}
+                  </h3>
+                </div>
+    
+                <div className='content'>
+                  <span>Data/Hora</span>
+                  <h3>
+                    {appointments.day}/{appointments.mounth} - {appointments.time}
+                  </h3>
+                </div>
+              </Patient>
+            )).reverse()
+          )
+        }
+      </DashContent>
+    </DashContainer>
   )
 };
 
---
+++ src/routes/Dashboard/style.ts
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+
+export const DashContainer = styled.section`
+  max-width: 1200px;
+  margin: 0 auto;
+`
+export const DashContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 80px;
+
+  h1 {
+    font-size: 2rem;
+    color: white;
+  }
+
+  .doctor-title {
+    margin-top: 5px;
+    font-size: 2rem;
+    color: white;
+  }
+
+  .no-content {
+    margin-top: 150px;
+    font-size: 1.2rem;
+    font-weight: 300;
+    opacity: 60%;
+  }
+
+`
+
+export const Patient = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  background: white;
+  padding: 20px;
+  border-radius: 20px;
+  margin-top: 50px;
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    margin: 15px 0;
+
+    h3 {
+      font-size: 1.5rem;
+      color: #0B181D;
+      margin-top: 5px;
+    }
+    
+    span {
+      font-size: 0.9rem;
+      color: #0B181D;
+      opacity: 80%;
+    }
+  }
+
+  .line {
+    width: 100%;
+    height: 1px;
+    background: #0B181D;
+    margin: 5px 0;
+  }
+`
