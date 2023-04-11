412722956ca67803511ce39846561017f91a8899
Pinelli <ricardosilvapinelli@gmail.com>
2023-04-11 01:11:29 -0300

    scroll bar: none

--- src/style/GlobalStyle.ts
+++ src/style/GlobalStyle.ts
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-  * { padding: 0; margin: 0; box-sizing: border-box;}
+  * { padding: 0; margin: 0; box-sizing: border-box; ::-webkit-scrollbar{ display: none}}
 
   body {
     font-family: 'Inter', sans-serif;
